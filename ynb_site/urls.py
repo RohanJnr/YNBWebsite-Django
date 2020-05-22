e7592a19719219a9aaa339c6b0edb6228470331a
rohanjnr <roalleti@gmail.com>
2020-05-23 00:59:20 +0530

    change static root and url file.

--- ynb_site/urls.py
+++ ynb_site/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,6 @@ urlpatterns = [
     path("", include("ynb_site.apps.minecraft.urls")),
     path("", include("ynb_site.apps.gallery.urls")),
 ]
-
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=set tings.MEDIA_ROOT)
