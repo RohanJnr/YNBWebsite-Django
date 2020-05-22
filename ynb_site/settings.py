d1408e7b480e72a3577cac87a120e8bf8cd678c9
rohanjnr <roalleti@gmail.com>
2020-05-23 00:55:29 +0530

    change static root.

--- ynb_site/settings.py
+++ ynb_site/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "ynb_site", "static_production")
+STATIC_ROOT = "static_production"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "ynb_site", "static")
