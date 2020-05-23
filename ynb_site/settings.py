47ac44738d4ab3e629a0f21a12de832e16d9da53
rohanjnr <roalleti@gmail.com>
2020-05-23 09:22:17 +0530

    allow domain name.

--- ynb_site/settings.py
+++ ynb_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("SETTING_DEBUG"))
 
-ALLOWED_HOSTS = ["localhost", os.getenv("SERVER_IP")]
+ALLOWED_HOSTS = ["localhost", os.getenv("SERVER_IP"), "youneedbackup.com", "www.youneedbackup.com"]
 
 
 # Application definition
