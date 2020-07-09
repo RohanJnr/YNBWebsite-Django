fece5ab9d4e3a70d4a622d0220371edb6b294d86
rohanjnr <roalleti@gmail.com>
2020-07-09 16:55:14 +0530

    youtube push notification callback url DONE

--- ynb_site/apps/api/views.py
+++ ynb_site/apps/api/views.py
@@ -1,3 +1,7 @@
+import json
+import os
+import requests
+
 from django.http import HttpResponse
 
 from rest_framework.views import APIView
@@ -24,7 +28,7 @@ class YtPushNotification(APIView):
 		for entry in root.findall('xmlns:entry', namespaces=namespaces):
 			link = entry.find('xmlns:link', namespaces=namespaces).get('href')
 
-		print(link)
+		self.notify_discord(link)
 			
 		return Response("Received", status=status.HTTP_200_OK)
 
@@ -33,3 +37,20 @@ class YtPushNotification(APIView):
 		hub_challenge = request.query_params["hub.challenge"]
 		print(hub_challenge)
 		return Response(int(hub_challenge), status=status.HTTP_200_OK)
+
+	@staticmethod
+	def notify_discord(link):
+		url = os.getenv("YT_DISCORD_WEBHOOK")
+
+		data = {}
+		data["content"] = "New youtube Vid!\nhttps://www.youtube.com/watch?v=gNW-KNgBO3o"
+		data["username"] = "IceBot"
+
+		r = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+		try:
+		    r.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+		    print(str(err))
+		else:
+		    print(f"Notification sent with status code {r.status_code}")
